00c51da37e608c42659322cdb259698f213b04eb
Vernal Liu <covernal@hotmail.com>
2017-05-07 13:59:54 +0800

    updated childAgeGroup selectbox as disabled

--- src/components/Widgets/LeduInput/AgesRangeSelector.js
+++ src/components/Widgets/LeduInput/AgesRangeSelector.js
@@ -4,7 +4,7 @@ class AgesRangeSelector extends React.Component{
   render() {
     return (
       <select className="form-control selectpicker show-tick" required={this.props.required} disabled={(this.props.disabled) ? true : false} data-style="btn-default" value={this.props.value} onChange={this.props.handleChange}>
-        <option value="">{this.props.placeholder}</option>
+        <option value="" disabled>{this.props.placeholder}</option>
         <option value="3-5岁">3-5岁</option>
         <option value="6-8岁">6-8岁</option>
         <option value="9-12岁">9-12岁</option>
