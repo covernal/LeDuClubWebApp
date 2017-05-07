a2b808d23bdcaff8d957131a722974c20d830d66
Vernal Liu <covernal@hotmail.com>
2017-05-07 13:29:36 +0800

    Updated members list page

--- src/components/Layouts/Admin/MembersList.js
+++ src/components/Layouts/Admin/MembersList.js
@@ -12,7 +12,7 @@ class MembersList extends React.Component{
       if(member.membershipStatus === "waitingForApproval") {
         operation = (<ul className="dropdown-menu"> <li><a onClick={()=>this.props.adminApproveMemberApplication(member.objectId)}>批准会员</a></li> </ul>);  
       }else {
-        let startExpLink = (member.allowTrial === true) ? <li><a onClick={()=>this.props.adminConfirmMemberStartTrial(member.objectId)}>开始体验</a></li> : '';
+        let startExpLink = (member.allowTrial === true && member.membershipStatus === "confirmed" && member.type === "member") ? <li><a onClick={()=>this.props.adminConfirmMemberStartTrial(member.objectId)}>开始体验</a></li> : '';
         operation = (
           <ul className="dropdown-menu">
             {(parseInt(member.deposit, 10) > 0) ? '' : <li><a onClick={()=>this.props.adminConfirmMemberDeposit(member.objectId)}>押金支付确认</a></li>}
